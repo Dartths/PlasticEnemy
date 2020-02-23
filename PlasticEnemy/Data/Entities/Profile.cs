5371ae9d5b6495c122803e427a936871c536ebc3
Molly Candon <S00191042@mail.itsligo.ie>
2020-02-23 19:10:21 +0000

    Starting db tables

---
+++ PlasticEnemy/Data/Entities/Profile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlasticEnemy.Data.Entities
+{
+    public class Profile
+    {
+        public Guid ProfileId { get; set; }
+    }
+}
