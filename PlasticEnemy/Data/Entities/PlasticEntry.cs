882494a5672ab71c07d463d3febb609d552b817a
Molly Candon <S00191042@mail.itsligo.ie>
2020-02-23 21:16:53 +0000

    Started Pastic Entry table

---
+++ PlasticEnemy/Data/Entities/PlasticEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlasticEnemy.Data.Entities
+{
+    public class PlasticEntry
+    {
+        public Guid PlasticEntryId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string PlasticTypeId { get; set; }
+
+        public int Size { get; set; }
+
+        public string ActionID { get; set; }
+    }
+}
