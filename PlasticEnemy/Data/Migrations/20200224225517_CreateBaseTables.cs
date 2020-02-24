9d3650e9358847eac84cd1c96a6d099f6516e5bf
Molly Candon <S00191042@mail.itsligo.ie>
2020-02-24 23:02:10 +0000

    Ran migrations

---
+++ PlasticEnemy/Data/Migrations/20200224225517_CreateBaseTables.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlasticEnemy.Data.Migrations
+{
+    public partial class CreateBaseTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actions",
+                columns: table => new
+                {
+                    ActionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ActionName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actions", x => x.ActionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Goals",
+                columns: table => new
+                {
+                    GoalID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GoalName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goals", x => x.GoalID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pictures",
+                columns: table => new
+                {
+                    PictureId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PictureType = table.Column<bool>(nullable: false),
+                    PictureLocation = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures", x => x.PictureId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlasticTypes",
+                columns: table => new
+                {
+                    PlasticTypeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlasticTypes", x => x.PlasticTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    ProfileId = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    DisplayName = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    County = table.Column<string>(nullable: true),
+                    DOB = table.Column<DateTime>(nullable: false),
+                    GoalId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.ProfileId);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Goals_GoalId",
+                        column: x => x.GoalId,
+                        principalTable: "Goals",
+                        principalColumn: "GoalID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlasticEntries",
+                columns: table => new
+                {
+                    PlasticEntryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfileId = table.Column<Guid>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    PlasticTypeId = table.Column<int>(nullable: false),
+                    Size = table.Column<string>(nullable: true),
+                    ActionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlasticEntries", x => x.PlasticEntryId);
+                    table.ForeignKey(
+                        name: "FK_PlasticEntries_Actions_ActionId",
+                        column: x => x.ActionId,
+                        principalTable: "Actions",
+                        principalColumn: "ActionId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlasticEntries_PlasticTypes_PlasticTypeId",
+                        column: x => x.PlasticTypeId,
+                        principalTable: "PlasticTypes",
+                        principalColumn: "PlasticTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlasticEntries_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "ProfileId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlasticEntries_ActionId",
+                table: "PlasticEntries",
+                column: "ActionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlasticEntries_PlasticTypeId",
+                table: "PlasticEntries",
+                column: "PlasticTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlasticEntries_ProfileId",
+                table: "PlasticEntries",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_GoalId",
+                table: "Profiles",
+                column: "GoalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pictures");
+
+            migrationBuilder.DropTable(
+                name: "PlasticEntries");
+
+            migrationBuilder.DropTable(
+                name: "Actions");
+
+            migrationBuilder.DropTable(
+                name: "PlasticTypes");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+
+            migrationBuilder.DropTable(
+                name: "Goals");
+        }
+    }
+}
