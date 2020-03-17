ae3b0656815759e5467a125cf58d8c10ef18d4f4
Philip Candon <darths.philip@gmail.com>
2020-03-17 19:57:44 +0000

    Creating User repository

---
+++ PlasticEnemy/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using PlasticEnemy.Data.Entities;
+
+namespace PlasticEnemy.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateUser(User user);
+        Task<User> GetUser(Guid userId);
+    }
+}
---
+++ PlasticEnemy/Data/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlasticEnemy.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlasticEnemy.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DbContext _peContext;
+
+        public UserRepository(DbContext pEContext)
+        {
+            _peContext = pEContext ?? throw new ArgumentNullException(nameof(pEContext));
+        }
+
+
+        public async Task<User> CreateUser(User user)
+        {
+            await _peContext.AddAsync(user);
+            await _peContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<User> GetUser(Guid userId)
+        {
+            return await _peContext.FindAsync<User>(userId);
+        }
+    }
+}
